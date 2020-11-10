a25ff2c9fd0db56b7c585a0da8452d9a2b396625
Ruediger Küpper <ruediger@kuepper.nrw>
2020-11-10 23:17:37 +0100

    cleanup

--- api/app.py
+++ api/app.py
@@ -215,10 +215,6 @@ def profileVerify(id,hash):
         result = "True"
     elif mydoc == 0:
         result = "False"
-    # list_cur = list(mydoc)
-    # showverify = list_cur[0]["verify"]
-    # json_data = dumps(list_cur, indent = 2, default=str)
-    # print ("json: " + json_data)
     return result, 200
 
 @app.route('/api/v1/domainadmin/sslexpire/<hostname>', methods=['GET'])
@@ -267,7 +263,6 @@ def profilExtrasUpdate(content):
     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
     mydb = myclient["domainadmin"]
     mycol = mydb["profile"]
-    # print ("Test: " + content['id'])
     ObjId = content['id']
     x = mycol.update_one(
         { "userid" : ObjId },
@@ -280,7 +275,6 @@ def profilUpdateField(id,key,value):
     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
     mydb = myclient["domainadmin"]
     mycol = mydb["profile"]
-    # print ("Test: " + content['id'])
     ObjId = content['id']
     ObjId = id
     x = mycol.update_one(
@@ -302,121 +296,3 @@ app.jinja_env.filters['tojson_pretty'] = to_pretty_json
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0', port=4006)
-
-
-# @app.route('/api/v1/marketplace/open', methods=["GET"])
-# def marktplace_open():
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marketplace"]
-#     mycol = mydb["offers"]
-#     mydoc = mycol.find({"active": "1"})
-#     list_cur = list(mydoc)
-#     json_data = dumps(list_cur, indent = 2, default=str)
-#     return json_data, 200
-
-# @app.route('/api/v1/marketplace/my/<id>', methods=["GET"])
-# def marktplace_my(id):
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marketplace"]
-#     mycol = mydb["offers"]
-#     # s = "\"seller\": " + id
-#     mydoc = mycol.aggregate( [ { "$match" : { "seller" : id } }, { "$project": {"id": {"$toString": '$_id' }, "title": "$title", "seller": "$seller", "price": "$price", "type": "$type", "category": "$category", "type": "$type", "image": "$image", "description": "$description", "active":"$active"} } ] )
-#     # mydoc = mycol.aggregate( [ { "$match" : { "_id" : ObjectId(id) } }, { "$project": {"id": {"$toString": '$_id' }, "title": "$title", "seller": "$seller", "price": "$price", "type": "$type", "category": "$category", "type": "$type", "image": "$image", "description": "$description", "active":"$active"} } ] )
-#     list_cur = list(mydoc)
-#     json_data = dumps(list_cur, indent = 2, default=str)
-#     return json_data, 200
-
-# @app.route('/api/v1/marketplace/myone/<id>', methods=["GET"])
-# def marktplace_myoffer(id):
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marketplace"]
-#     mycol = mydb["offers"]
-#     # s = "\"seller\": " + id
-#     # mydoc = mycol.aggregate( [ { "$match" : { "seller" : id } }, { "$project": {"id": {"$toString": '$_id' }, "title": "$title", "seller": "$seller", "price": "$price", "type": "$type", "category": "$category", "type": "$type", "image": "$image", "description": "$description", "active":"$active"} } ] )
-#     mydoc = mycol.aggregate( [ { "$match" : { "_id" : ObjectId(id) } }, { "$project": {"id": {"$toString": '$_id' }, "title": "$title", "seller": "$seller", "price": "$price", "type": "$type", "category": "$category", "type": "$type", "image": "$image", "description": "$description", "active":"$active"} } ] )
-#     list_cur = list(mydoc)
-#     json_data = dumps(list_cur, indent = 2, default=str)
-#     return json_data, 200
-
-# @app.route('/api/v1/marketplace/active', methods=["GET"])
-# def marktplace_aktive():
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marketplace"]
-#     mycol = mydb["offers"]
-#     mydoc = mycol.aggregate([ { "$match" : { "active" : "1" } },{ "$project": {"id": {"$toString": '$_id' }, "title": "$title", "seller": "$seller", "price": "$price", "type": "$type", "category": "$category", "type": "$type", "image": "$image", "description": "$description", "active":"$active"} } ] )
-#     list_cur = list(mydoc)
-#     json_data = dumps(list_cur, indent = 2, default=str)
-#     return json_data, 200
-
-# @app.route('/api/v1/marketplace/categoryactive/<id>', methods=["GET"])
-# def category_aktive(id):
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marketplace"]
-#     mycol = mydb["offers"]
-#     mydoc = mycol.aggregate([ { "$match" : { "active" : "1", "category": id } },{ "$project": {"id": {"$toString": '$_id' }, "title": "$title", "seller": "$seller", "price": "$price", "type": "$type", "category": "$category", "type": "$type", "image": "$image", "description": "$description", "active":"$active"} } ] )
-#     list_cur = list(mydoc)
-#     json_data = dumps(list_cur, indent = 2, default=str)
-#     return json_data, 200
-
-# @app.route('/api/v1/markplace/<id>', methods=["GET"])
-# def marktplaceById(id):
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marktplace"]
-#     mycol = mydb["offer"]
-#     s = "\"_id\": ObjectId('" + id + "')"
-#     mydoc = mycol.aggregate([
-#       { "$match": { s } },
-#     ])
-#     list_cur = list(mydoc)
-#     json_data = dumps(list_cur, indent = 2, default=str)
-#     return json_data, 200
-
-# @app.route('/api/v1/marketplace/<id>', methods=["GET"])
-# def marktplaceOffer(id):
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marketplace"]
-#     mycol = mydb["offers"]
-#     mydoc = mycol.find({"_id": ObjectId(id) })
-#     list_cur = list(mydoc)
-#     json_data = dumps(list_cur, indent = 2, default=str)
-#     return json_data, 200
-#
-# @app.route('/api/v1/marketplace/categories', methods=["GET"])
-# def marktplaceCategories():
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marketplace"]
-#     mycol = mydb["categories"]
-#     mydoc = mycol.aggregate([ { "$project": {"id": {"$toString": '$_id' }, "active": "$active", "name": "$name" } } ] )
-#     list_cur = list(mydoc)
-#     json_data = dumps(list_cur, indent = 2, default=str)
-#     return json_data, 200
-#
-# @app.route('/api/v1/marketplace/categories/<id>', methods=["GET"])
-# def marktplaceCategoriesId(id):
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marketplace"]
-#     mycol = mydb["categories"]
-#     mydoc = mycol.find({"_id": ObjectId(id)})
-#     list_cur = list(mydoc)
-#     cat_name = list_cur[0]["name"]
-#     json_data = dumps(list_cur, indent = 2, default=str)
-#     return cat_name, 200
-#
-# @app.route('/api/v1/marketplace/uploads/<seller>/<filename>', methods=["GET"])
-# def save_upload(seller,filename):
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marketplace"]
-#     mycol = mydb["uploads"]
-#     mydict = { "seller": seller, "filename": filename }
-#     x = mycol.insert_one(mydict)
-#     return jsonify({"result":"ok - "}), 200
-#
-# @app.route('/api/v1/marketplace/uploads/<seller>', methods=["GET"])
-# def getUploadBySeller(seller):
-#     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
-#     mydb = myclient["marketplace"]
-#     mycol = mydb["uploads"]
-#     mydoc = mycol.find({"seller": seller})
-#     list_cur = list(mydoc)
-#     json_data = dumps(list_cur, indent = 2, default=str)
-#     return json_data, 200
--- app.py
+++ app.py
@@ -18,17 +18,11 @@ import ssl
 import datetime
 import sys
 
-
-UPLOAD_FOLDER = '/app/static/uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-# MyAnonymousUser = "Nobody"
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
     'db': 'domainadmin',
-    # 'host': 'mongodb://',
     'host': 'mongodb://mongo/domainadmin?retryWrites=true&w=majority'
 }
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db = MongoEngine(app)
 app.config["SECRET_KEY"] = str(os.environ.get("SECRET_KEY"))
@@ -36,15 +30,12 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-# login_manager.anonymous_user = MyAnonymousUser
 
 class User(UserMixin, db.Document):
     meta = {'collection': 'users'}
     email = db.StringField(max_length=50)
     password = db.StringField()
     userlevel = db.StringField()
-    # name = db.StringField()
-    # meta = {'strict': False}
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -53,15 +44,10 @@ def load_user(user_id):
 class RegForm(FlaskForm):
     email = StringField('email',  validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=20)])
-    # name = StringField('name', validators=[InputRequired(),Length(min=3, max=30)])
 
 @app.route('/')
 @login_required
 def index():
-    # data = read_categories()
-    # categories = {}
-    # for d in data:
-    #     categories[id] = d['name']
     profileExtras = getProfileExtras()
     for i in profileExtras:
         name=i['name']
@@ -71,11 +57,7 @@ def index():
           userlevel=current_user.userlevel,
           userid=current_user.id,
           data=read_domains(),
-          # categories=categories,
-          # cats=read_categories(),
-          # categories=read_categories(),
           name=name,
-          # userlist=read_userlist(),
           loggedIn = "1",
         )
     else:
@@ -93,9 +75,6 @@ def dashboardProfil():
     profileExtras = getProfileExtras()
     for i in profileExtras:
         name=i['name']
-    # print ("Name: " + str(profileExtras) ) # + " test: " + profileExtras)
-    # x = dict(profileExtras)
-    # print ("X: ", x['name'])
     return render_template('profil.html',
       email=current_user.email,
       password=current_user.password,
@@ -103,7 +82,6 @@ def dashboardProfil():
       userid=current_user.id,
       profileExtras=profileExtras,
       name=name,
-      # categories=read_categories(),
       loggedIn = loggedIn
     )
 
@@ -127,7 +105,6 @@ def dashboardProfilSave():
 
 @app.route('/newdomain', methods=['POST'])
 def addItemSave():
-    # print ("Title: " + request.form['title'])
     domain = str(request.form['domain'])
     description = request.form['description']
     registrar = request.form['registrar']
@@ -165,9 +142,7 @@ def viewOffer(id):
     profileExtras = getProfileExtras()
     for i in profileExtras:
         name=i['name']
-    # categories = read_categories()
     data = read_domain_data(id)
-    # print ("data: " + str(data))
     return render_template('domain.html',
       name=name,
       pwhash=current_user.password,
@@ -175,7 +150,6 @@ def viewOffer(id):
       userid=current_user.id,
       data=data,
       id=id,
-      # categories=categories,
       userlist=read_userlist(),
       loggedIn=loggedIn
     )
@@ -187,7 +161,6 @@ def sslcheckAll():
     for data in domains:
         print ("Domain: " + data['domain'])
         sslcheck(data['domain'])
-        # /api/v1/domainadmin/sslexpire/<hostname>
     return redirect(url_for('sslexpirelist'))
 
 @app.route('/sslexpirelist', methods=['GET'])
@@ -208,46 +181,6 @@ def sslexpirelist():
       loggedIn = loggedIn
     )
 
-def getsslexpirelist():
-    data = {}
-    import urllib, json
-    import urllib.request
-    apiurl = "http://api:4006/api/v1/domainadmin/sslexpire/list"
-    response = urllib.request.urlopen(apiurl)
-    data = json.loads(response.read())
-    return data
-
-# @app.route('/category/<id>', methods=['GET'])
-# @login_required
-# def categoryId(id):
-#     data = read_categories()
-#     categories = {}
-#     for d in data:
-#         categories[id] = d['name']
-#     profileExtras = getProfileExtras()
-#     for i in profileExtras:
-#         name=i['name']
-#     if current_user.is_authenticated:
-#         return render_template('index.html',
-#           pwhash=current_user.password,
-#           userlevel=current_user.userlevel,
-#           userid=current_user.id,
-#           # data=read_markedplace_active(),
-#           data=read_category_active(id),
-#           # categories=categories,
-#           # cats=read_categories(),
-#           categories=read_categories(),
-#           name=name,
-#           userlist=read_userlist(),
-#           loggedIn = "1",
-#         )
-#     else:
-#         return render_template('index.html',
-#             loggedIn = "0",
-#             data=read_markedplace_active(),
-#             categories=categories,
-#         )
-
 @app.route('/dashboard')
 @login_required
 def dashboard():
@@ -258,42 +191,13 @@ def dashboard():
     for i in profileExtras:
         if i['name']:
             name=i['name']
-    # data = read_categories()
-    # categories = {}
-    # for d in data:
-    #     categories[id] = d['name']
     return render_template('dashboard.html',
       name=name,
       pwhash=current_user.password,
       userlevel=current_user.userlevel,
       userid=current_user.id,
-      # data=read_markedplace(),
-      data=read_marketplace_my(),
-      # categories=categories,
-      # categories=read_categories(),
+      data=read_domains(),
       userlist=read_userlist(),
-      loggedIn=loggedIn
-      # uploads=read_uploads()
-    )
-
-@app.route('/dashboard/offer/<id>')
-@login_required
-def dashboardOffer(id):
-    if current_user.is_authenticated:
-        loggedIn = "1"
-    profileExtras = getProfileExtras()
-    for i in profileExtras:
-        name=i['name']
-    data = read_markedplace_myoffer(id)
-    return render_template('dashboard_offer.html',
-      name=name,
-      pwhash=current_user.password,
-      userlevel=current_user.userlevel,
-      userid=current_user.id,
-      data=data,
-      id=id,
-      uploads=read_uploads(),
-      categories=read_categories(),
       loggedIn=loggedIn
     )
 
@@ -305,9 +209,7 @@ def dashboardOfferEdit(id):
     profileExtras = getProfileExtras()
     for i in profileExtras:
         name=i['name']
-    # categories = read_categories()
     data = read_domain_data(id)
-    # print ("data: " + str(data))
     return render_template('editdomain.html',
       name=name,
       pwhash=current_user.password,
@@ -315,9 +217,6 @@ def dashboardOfferEdit(id):
       userid=current_user.id,
       data=data,
       id=id,
-      # categories=categories,
-      # userlist=read_userlist(),
-      # uploads=read_uploads(),
       loggedIn=loggedIn
     )
 
@@ -346,72 +245,6 @@ def dashboardOfferSave(id):
     r = requests.post(apiurl, json=dumps(data), headers=headers)
     return redirect(url_for('index'))
 
-# @app.route('/domain/sslexpire/<hostname>', methods=['GET'])
-# @login_required
-# def ssl_expiry_datetime(hostname):
-#     now = datetime.datetime.now()
-#     ssl_dateformat = r'%b %d %H:%M:%S %Y %Z'
-#     context = ssl.create_default_context()
-#     context.check_hostname = False
-#     conn = context.wrap_socket(
-#         socket.socket(socket.AF_INET),
-#         server_hostname=hostname,
-#     )
-#     conn.settimeout(5.0)
-#     conn.connect((hostname, 443))
-#     ssl_info = conn.getpeercert()
-#     expire = datetime.datetime.strptime(ssl_info['notAfter'], ssl_dateformat)
-#     # expire = ssl_expiry_datetime(domain)
-#     diff = expire - now
-#     # print ("Domain name: {} Expiry Date: {} Expiry Day: {}".format(domain,expire.strftime("%Y-%m-%d"),diff.days))
-#     return print ("Expiry Date: {} Expiry Day: {}".format(expire.strftime("%Y-%m-%d"),diff.days))
-#     # return datetime.datetime.strptime(ssl_info['notAfter'], ssl_dateformat)
-
-# if __name__ == "__main__":
-#     now = datetime.datetime.now()
-#     try:
-#         expire = ssl_expiry_datetime(domain)
-#         diff = expire - now
-#         # print ("Domain name: {} Expiry Date: {} Expiry Day: {}".format(domain,expire.strftime("%Y-%m-%d"),diff.days))
-#         print ("Expiry Date: {} Expiry Day: {}".format(expire.strftime("%Y-%m-%d"),diff.days))
-#     except Exception as e:
-#         print (e)
-
-
-
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload_file():
-#     if current_user.is_authenticated:
-#         loggedIn = "1"
-#     profileExtras = getProfileExtras()
-#     for i in profileExtras:
-#         name=i['name']
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # if user does not select file, browser also
-#         # submit an empty part without filename
-#         if file.filename == '':
-#             flash('No selected file')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             filename = str(current_user.id) + "_" + str(time.time()) + "_" + str(file.filename)
-#             print ("Filename: " + filename)
-#             save_upload(filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#             return redirect(url_for('upload_file',
-#                                     filename=filename))
-#     return render_template('upload.html',
-#         uploads=read_uploads(),
-#         loggedIn=loggedIn,
-#         categories=read_categories(),
-#         name=name
-#     )
-
 @app.route('/logout', methods = ['GET'])
 @login_required
 def logout():
@@ -428,17 +261,14 @@ def register():
             existing_user = User.objects(email=form.email.data).first()
             if existing_user is None:
                 hashpass = generate_password_hash(form.password.data, method='sha256')
-                # hey = User(form.email.data,hashpass).save()
                 hey = User(email=form.email.data,password=hashpass).save()
                 login_user(hey)
                 iddata = getProfileId(form.email.data)
                 for i in iddata:
                     id=i['id']
-                print ("ID: " + id )
                 verifyhash = generate_password_hash(id, method='sha256')
                 profileFieldUpdate(id,'verify',verifyhash)
                 profileFieldUpdate(id,'name',id)
-                # sendVerifyMail(id,verifyhash,form.email.data)
                 return redirect(url_for('index'))
         else:
             print ("Nicht validiert.")
@@ -446,7 +276,6 @@ def register():
 
 @app.route('/verify/<id>/<hash>', methods=['GET'])
 def verifyhash(id,hash):
-    # userid verify
     data = {}
     import urllib, json
     import urllib.request
@@ -457,33 +286,16 @@ def verifyhash(id,hash):
 
 @app.route('/register/test', methods=['GET', 'POST'])
 def registerTest():
-    # form = RegForm()
     existing_user = User.objects(email='test@9it.de').first()
     if existing_user is None:
         hashpass = generate_password_hash('oldwinnt', method='sha256')
         hey = User(email='test@9it.de',password=hashpass).save()
-        # login_user(hey)
         iddata = getProfileId('test@9it.de')
         for i in iddata:
             id=i['id']
         print ("ID: " + id )
         profileFieldUpdate(id,'verify',generate_password_hash(id, method='sha256'))
     return ("ID: " + id )
-
-    # if request.method == 'POST':
-    #     print ("Mail: " + form.email.data)
-    #     if form.validate():
-    #         existing_user = User.objects(email=form.email.data).first()
-    #         if existing_user is None:
-    #             hashpass = generate_password_hash(form.password.data, method='sha256')
-    #             # hey = User(form.email.data,hashpass).save()
-    #             hey = User(email=form.email.data,password=hashpass).save()
-    #             login_user(hey)
-    #             id = getProfileId(form.email.data)
-    #             profileFieldUpdate(id['id'],'verify',generate_password_hash(id['id'], method='sha256'))
-    #             return redirect(url_for('dashboard'))
-    # return render_template('register.html', form=form)
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -501,28 +313,6 @@ def login():
                     return redirect(url_for('index'))
     return render_template('login.html', form=form, loggedIn=loggedIn)
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def save_upload(filename):
-    data = {}
-    import urllib, json
-    import urllib.request
-    apiurl = "http://api:4006/api/v1/marketplace/uploads/" + str(current_user.id) + "/" + filename
-    response = urllib.request.urlopen(apiurl)
-    data = json.loads(response.read())
-    return data
-
-def read_markedplace():
-    data = {}
-    import urllib, json
-    import urllib.request
-    apiurl = "http://api:4006/api/v1/marketplace/" + str(current_user.id)
-    response = urllib.request.urlopen(apiurl)
-    data = json.loads(response.read())
-    return data
-
 def sslcheck(domain):
     data = {}
     print ("Check domain: " + domain)
@@ -533,29 +323,20 @@ def sslcheck(domain):
     data = json.loads(response.read())
     return data
 
+def getsslexpirelist():
+    data = {}
+    import urllib, json
+    import urllib.request
+    apiurl = "http://api:4006/api/v1/domainadmin/sslexpire/list"
+    response = urllib.request.urlopen(apiurl)
+    data = json.loads(response.read())
+    return data
+
 def read_domains():
     data = {}
     import urllib, json
     import urllib.request
     apiurl = "http://api:4006/api/v1/domainadmin/list"
-    response = urllib.request.urlopen(apiurl)
-    data = json.loads(response.read())
-    return data
-
-def read_category_active(id):
-    data = {}
-    import urllib, json
-    import urllib.request
-    apiurl = "http://api:4006/api/v1/marketplace/categoryactive/" + id
-    response = urllib.request.urlopen(apiurl)
-    data = json.loads(response.read())
-    return data
-
-def read_markedplace_open():
-    data = {}
-    import urllib, json
-    import urllib.request
-    apiurl = "http://api:4006/api/v1/marketplace/open"
     response = urllib.request.urlopen(apiurl)
     data = json.loads(response.read())
     return data
@@ -598,38 +379,11 @@ def profileFieldUpdate(id,key,value):
     data = json.loads(response.read())
     return data
 
-def read_categories():
-    data = {}
-    import urllib, json
-    import urllib.request
-    apiurl = "http://api:4006/api/v1/marketplace/categories"
-    response = urllib.request.urlopen(apiurl)
-    data = json.loads(response.read())
-    return data
-
 def read_domain_data(id):
     data = {}
     import urllib, json
     import urllib.request
     apiurl = "http://api:4006/api/v1/domainadmin/domain/" + id
-    response = urllib.request.urlopen(apiurl)
-    data = json.loads(response.read())
-    return data
-
-def read_marketplace_my():
-    data = {}
-    import urllib, json
-    import urllib.request
-    apiurl = "http://api:4006/api/v1/marketplace/my/" + str(current_user.id)
-    response = urllib.request.urlopen(apiurl)
-    data = json.loads(response.read())
-    return data
-
-def read_markedplace_myoffer(id):
-    data = {}
-    import urllib, json
-    import urllib.request
-    apiurl = "http://api:4006/api/v1/marketplace/myone/" + id
     response = urllib.request.urlopen(apiurl)
     data = json.loads(response.read())
     return data
@@ -643,15 +397,6 @@ def read_userlist():
     data = json.loads(response.read())
     return data
 
-
-def read_uploads():
-    data = {}
-    import urllib, json
-    import urllib.request
-    apiurl = "http://api:4006/api/v1/marketplace/uploads/" + str(current_user.id)
-    response = urllib.request.urlopen(apiurl)
-    data = json.loads(response.read())
-    return data
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0', port=4006)
